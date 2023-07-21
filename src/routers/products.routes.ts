318041de437f7635c98a6499cac0be894c26f0b5
Giuseppe <giuseppebrandalise@gmail.com>
2023-07-21 02:01:32 -0300

    Requisito 5

--- src/routers/products.routes.ts
+++ src/routers/products.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import productsMiddlewares from '../middlewares/products.middlewares';
 
 const productsRouter = Router();
 
-productsRouter.post('/', productsController.createProduct);
+productsRouter.post(
+  '/',
+  productsMiddlewares.verifyName,
+  productsMiddlewares.verifyPrice,
+  productsController.createProduct,
+);
 productsRouter.get('/', productsController.getAll);
 
 export default productsRouter;
